6a6f45ca38b0684637439694d068d5f68f421b19
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-18 10:08:26 -0600

    Init commit

---
+++ Bitrise Demo/Bitrise_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitrise_DemoApp.swift
+//  Bitrise Demo
+//
+//  Created by Frederick Javalera on 4/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Bitrise_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
